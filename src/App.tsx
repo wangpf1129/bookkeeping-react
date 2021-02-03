1df77e98c99eb9c510f6a781957fc67e7ea6399a
Wangpf <1204981117@qq.com>
2021-02-03 22:09:12 +0800

    remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import './App.css';
-import styled from 'styled-components';
 
-const Button = styled.button`
-  color:red;
-`
 function App() {
   return (
     <div>
-      App.
-      <Button>按钮</Button>
+      App.....
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
@@ -12,4 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals();
+// reportWebVitals();
