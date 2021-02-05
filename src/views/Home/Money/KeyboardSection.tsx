1be1d130757fa43023cf7e8a0eaee02782cbc93d
Wangpf <1204981117@qq.com>
2021-02-05 22:53:17 +0800

    money组件的基本功能也实现

---
+++ src/views/Home/Money/KeyboardSection.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import React from 'react';
+import {NotesSection} from './NotesSection';
+import {NumberPadSection} from './NumberPadSection';
+
+const Wrapper = styled.section`
+  background-color:#9ccac0;
+  box-shadow: 2px 2px 3px rgba(0,0,0,.8);
+`;
+
+type  Props = {
+  note:string,
+  amount:number,
+  onChangeNote:(note:string)=>void
+  onChangeAmount:(amount:number)=>void
+  onOk ?: ()=>void
+}
+const KeyboardSection:React.FC<Props>=(props)=>{
+  return (
+          <Wrapper>
+              <NotesSection {...props}/>
+              <NumberPadSection {...props}/>
+          </Wrapper>
+  )
+}
+
+export  {KeyboardSection}
---
+++ src/views/Home/Money/generateOutput.tsx
@@ -0,0 +1,60 @@
+const computeResult = (output: string) => {
+  let numArr: string[] = output.split(/[+-]/);  // 筛选出 数字
+  let symArr: string = output.replace(/[^+-]/g, '');  // 筛选出 运算符
+  let sum: number = Number(numArr[0]);
+
+  for (let i = 0; i < numArr.length - 1; i++) {
+    let symOne = symArr.charAt(i);
+    let num = Number(numArr[i + 1]);
+    if (symOne === '+') {
+      sum += num;
+    } else if (symOne === '-') {
+      sum -= num;
+    }
+  }
+  return sum.toFixed(2);
+};
+
+const generateOutput = (text: string, output: string) => {
+  switch (text) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      if (output === '0') {
+        return text;
+      } else {
+        return output + text;
+      }
+    case '.':
+      if (output[output.length - 1] === '.' ) return output;
+      return output + '.';
+    case'+':
+      if (output[output.length - 1] === '+' || output[output.length - 1] === '-') return output;
+      return output + '+';
+    case'-':
+      if (output[output.length - 1] === '+' || output[output.length - 1] === '-') return output;
+      return output + '-';
+    case'删除':
+      if (output.length === 1) {
+        return '';
+      } else {
+        return output.slice(0, -1) || '';
+      }
+    case '=':
+      return computeResult(output) + '';
+    case '完成':
+      return output;
+    default:
+      return '';
+  }
+};
+
+export default generateOutput;
+
